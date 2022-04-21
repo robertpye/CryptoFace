3b9af69d91618fe9f978ce283d8596ac094931f6
Nikolay Feldman <niko@nfeld.com>
2022-04-21 16:14:37 -0400

    ticker refresh refactor

--- app/message.js
+++ app/message.js
@@ -3,6 +3,9 @@ This file is common messaging functions specific to app.
 */
 
 import * as messaging from 'messaging'
+import { outbox } from 'file-transfer'
+
+const FILE_REQUEST_REFRESH = 'request_tick_refresh'
 
 export function sendValue(key, value) {
   if (messaging.peerSocket.readyState === 0) {
@@ -15,4 +18,13 @@ export function sendValue(key, value) {
   } else {
     console.log('No peerSocket connection')
   }
+}
+
+export function requestRefresh() {
+    const filename = + (Math.floor(Math.random() * 10000000000000000))
+    outbox.enqueue(filename, '').then(() => {
+        debug && console.log(`GA4: File ${filename} transferred successfully.`)
+    }).catch(function (error) {
+        debug && console.log(`GA4: File ${filename} failed to transfer.`)
+    })
 }
