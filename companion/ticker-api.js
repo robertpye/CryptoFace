55f2cd57d577544cd18209125034029f11b125f2
Nikolay Feldman <niko@nfeld.com>
2023-12-13 02:01:24 -0500

    change to chart api

--- companion/ticker-api.js
+++ companion/ticker-api.js
@@ -6,28 +6,36 @@ export function fetchTickers(tickers) {
         return Promise.reject(new Error('No tickers provided'))
     }
 
-    const url = `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${tickers}`
-    return fetch(url)
-        .then(response => response.json())
-        .then(json => {
-            if (json.quoteResponse.error) {
-                log.warn(`yfinance error ${json.quoteResponse.error}`) // this could be an error due to user input for tickers so log as debug. This isn't an app error
-                return null
-            }
-            const tickerValues = json.quoteResponse.result.map(data => {
-                const dashIndex = data.symbol.indexOf('-') // crypto
-                const ticker = dashIndex > -1 ? data.symbol.substring(0, dashIndex) : data.symbol
-                return {
-                    ticker,
-                    price: round(data.regularMarketPrice, 2),
-                    // change: round(data.regularMarketChange, 2),
-                    changePercent: round(data.regularMarketChangePercent, 2),
+    const tickerRequests = tickers.map(ticker => {
+        const url = `https://query1.finance.yahoo.com/v8/finance/chart/${ticker}?includePrePost=false&interval=2m&useYfid=true&range=1d`
+        return fetch(url)
+            .then(response => response.json())
+            .then(json => {
+                if (json.chart.error) {
+                    log.warn(`yfinance error`, json.chart.error) // this could be an error due to user input for tickers so log as debug. This isn't an app error
+                    return null
                 }
+                const tickerValues = json.chart.result.map(data => {
+                    const meta = data.meta
+                    const dashIndex = meta.symbol.indexOf('-') // crypto
+                    const ticker = dashIndex > -1 ? meta.symbol.substring(0, dashIndex) : meta.symbol
+                    const price = meta.regularMarketPrice
+                    const priceChangePercent = (price / meta.previousClose - 1) * 100
+                    return {
+                        ticker,
+                        price: round(price, 2),
+                        // change: round(data.regularMarketChange, 2),
+                        changePercent: round(priceChangePercent, 2),
+                    }
+                })
+                return tickerValues[0]
             })
-            return tickerValues
-        })
-        .catch(e => {
-            log.error('exception caught', e)
-            return null
-        })
+            .catch(e => {
+                log.error('exception caught', e)
+                return null
+            })
+    })
+
+
+    return Promise.all(tickerRequests)
 }
